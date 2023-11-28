82f74ad067553db4d46c889052d60e8030450da0
priyanshuMishraBackendDev <134582250+priyanshuMishraBackendDev@users.noreply.github.com>
2023-11-28 07:34:38 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'KLVKMdsrihfb20024e#fga.gdadggs';
 
 module.exports = async (req, res, db) => {
+  console.log("At line 5")
   const data = req.body
   const usersCollection = db.collection('users');
   const existingUser = await usersCollection.findOne({ email: data.email });
@@ -19,7 +20,7 @@ module.exports = async (req, res, db) => {
   }
 
   const accessToken = jwt.sign(user, secretKey, { expiresIn: '1h' });
-
+  console.log(accessToken)
   res.json({ message: 'Signup successful', accessToken });
 };
 
